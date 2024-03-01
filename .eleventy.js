7f395647e9a6793b9ff51c439dfc12e52fcb33b2
brian khuu <mofosyne@gmail.com>
2024-03-01 23:12:52 +1100

    subfolder support as well

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(syntaxHighlight);
-  
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
   // This is required for JSON Feed https://github.com/11ty/eleventy-plugin-rss/blob/master/sample/feed-json.11ty.js
   eleventyConfig.addJavaScriptFunction("absoluteUrl", pluginRss.absoluteUrl);
   eleventyConfig.addJavaScriptFunction("htmlToAbsoluteUrls", pluginRss.convertHtmlToAbsoluteUrls);
@@ -35,7 +36,7 @@ module.exports = function(eleventyConfig) {
       // If your site lives in a different subdirectory (particularly useful with GitHub pages), use pathPrefix to specify this. 
       // It’s used by the url filter and inserted at the beginning of all absolute url href links. 
       // It does not affect your file structure. Leading or trailing slashes are all normalized away, so don’t worry about it.
-      //pathPrefix: "/",
+      pathPrefix: "/call-to-action-map-static-site",
 
       // ⚠️ These values are both relative to your input directory.
       // ref: https://www.11ty.dev/docs/config/#directory-for-layouts-(optional)
