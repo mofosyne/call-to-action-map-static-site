48d5fb469d8934848bfdae24a7d5c2bf2d323b8f
brian khuu <mofosyne@gmail.com>
2024-03-01 23:26:26 +1100

    fix pathPrefix being in wrong location

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(syntaxHighlight);
-  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
   // This is required for JSON Feed https://github.com/11ty/eleventy-plugin-rss/blob/master/sample/feed-json.11ty.js
   eleventyConfig.addJavaScriptFunction("absoluteUrl", pluginRss.absoluteUrl);
@@ -31,12 +30,12 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setFrontMatterParsingOptions({ excerpt: true});
 
   return {
+    // If your site lives in a different subdirectory (particularly useful with GitHub pages), use pathPrefix to specify this. 
+    // It’s used by the url filter and inserted at the beginning of all absolute url href links. 
+    // It does not affect your file structure. Leading or trailing slashes are all normalized away, so don’t worry about it.
+    pathPrefix: "/call-to-action-map-static-site/",
+  
     dir: {
-
-      // If your site lives in a different subdirectory (particularly useful with GitHub pages), use pathPrefix to specify this. 
-      // It’s used by the url filter and inserted at the beginning of all absolute url href links. 
-      // It does not affect your file structure. Leading or trailing slashes are all normalized away, so don’t worry about it.
-      pathPrefix: "/call-to-action-map-static-site",
 
       // ⚠️ These values are both relative to your input directory.
       // ref: https://www.11ty.dev/docs/config/#directory-for-layouts-(optional)
